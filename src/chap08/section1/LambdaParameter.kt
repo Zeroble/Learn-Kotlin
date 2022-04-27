55382cea9824ea52044bbb1489b7e7209a786073
Zeroble <dlalsdyd0219@gmail.com>
2022-04-27 20:24:54 +0900

    -> p.358

---
+++ src/chap08/section1/LambdaParameter.kt
@@ -0,0 +1,13 @@
+package chap08.section1
+
+fun <T> add(a: T, b: T, op: (T, T) -> T): T {
+    return op(a, b)
+}
+
+fun main() {
+    val sum1: (Int, Int) -> Int = { a, b -> a + b }
+    val sum2 = { a: Int, b: Int -> a + b }
+    println(add(2, 3) { a, b -> a + b })
+    println(add(2,3, sum1))
+    println(add(2,3, sum2))
+}
